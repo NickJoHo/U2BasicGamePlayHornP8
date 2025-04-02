3fe7f0ca55f12ed86a7f21ac31c05fcc8b49fbcc
Rafaelakamexican <739415@student.dusd.net>
2025-04-02 15:32:26 -0700

    4/2/25
    finished lesson 2.4

--- BasicGameplayU2HornP8/Assets/Scripts/DestroyOutOfBounds.cs
+++ BasicGameplayU2HornP8/Assets/Scripts/DestroyOutOfBounds.cs
@@ -14,11 +14,15 @@ public class DestroyOutOfBounds : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // if an object goes pAST the players view in the game remove the object
         if (transform.position.z > topBound)
         {
             Destroy(gameObject);
-        } else if (transform.position.z < lowerBound) { 
-            Destroy(gameObject); }
+        } else if (transform.position.z < lowerBound) 
+        {
+            Debug.Log("Game Over!");
+            Destroy(gameObject); 
+        }
     }
 }
 
--- BasicGameplayU2HornP8/Assets/Scripts/SpawnManager.cs
+++ BasicGameplayU2HornP8/Assets/Scripts/SpawnManager.cs
@@ -7,7 +7,7 @@ public class SpawnManager : MonoBehaviour
     public GameObject[] animalPrefabs;
     private float spawnRangeX = 20;
     private float spawnPosZ = 20;
-    private float startDelay = 2;
+    private float startDelay = 1;
     private float spawnInterval = 1.5f;
 
     // Start is called before the first frame update
@@ -25,9 +25,10 @@ public class SpawnManager : MonoBehaviour
     void SpawnRandomAnimal()
     {
         int animalIndex = Random.Range(0, animalPrefabs.Length);
-        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+        Vector3 spawnpos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
 
-        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
+        Instantiate(animalPrefabs[animalIndex], spawnpos, animalPrefabs[animalIndex].transform.rotation);
     }
 }
+
         
