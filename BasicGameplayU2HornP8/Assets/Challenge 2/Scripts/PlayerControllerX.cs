f03188872fed94da9225a646fe9d7fe7d52fbe14
Rafaelakamexican <739415@student.dusd.net>
2025-04-22 14:06:18 -0700

    idk
    idk

---
+++ BasicGameplayU2HornP8/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private Boolean canSpawnDog = true;
+    void Start()
+    {
+        InvokeRepeating("ResetTimer", 1.0f, 1.0f);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && canSpawnDog)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            canSpawnDog = false;
+        }
+    }
+    void ResetTimer()
+    {
+        canSpawnDog = true;
+    }
+}
